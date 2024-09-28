c8942dbf172392067d7e7eb56a3543f82db6fa76
Administrator <example@example.com>
2024-09-28 22:49:32 +0800

    更新改进了组织结构修复全局数组的问题

--- src/main/java/top/voidc/ir/ice/instruction/IceAllocaInstruction.java
+++ src/main/java/top/voidc/ir/ice/instruction/IceAllocaInstruction.java
@@ -1,11 +1,12 @@
 package top.voidc.ir.ice.instruction;
 
+import top.voidc.ir.IceBlock;
 import top.voidc.ir.ice.type.IcePtrType;
 import top.voidc.ir.ice.type.IceType;
 
 public class IceAllocaInstruction extends IceInstruction {
-    public IceAllocaInstruction(String name, IceType type) {
-        super(name, new IcePtrType<>(type));
+    public IceAllocaInstruction(IceBlock parent, String name, IceType type) {
+        super(parent, name, new IcePtrType<>(type));
         setInstructionType(InstructionType.ALLOCA);
     }
 
---
+++ src/main/java/top/voidc/misc/annotation/Use.java
@@ -0,0 +1,11 @@
+package top.voidc.misc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.PARAMETER)
+public @interface Use {
+}
