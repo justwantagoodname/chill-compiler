2784472ca5668c5d0f71bdb2e2c76f2463f32f52
null <30485571+nullnan@users.noreply.github.com>
2025-04-04 20:50:57 +0800

    更新Flag 的一点小问题

--- src/main/java/Compiler.java
+++ src/main/java/Compiler.java
@@ -10,10 +10,8 @@ import top.voidc.misc.Log;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 public class Compiler {
-    private static final Logger LOGGER = Logger.getLogger(Log.class.getName());
     public static void main(String[] args) throws IOException {
         Flag.init(args);
         final String sourcePath = Flag.get("source");
@@ -34,15 +32,21 @@ public class Compiler {
 
         generator.generateIR();
 
-        Log.d('\n' + context.getCurrentIR().toString());
-
         final String outputPath = Flag.get("-o");
         Log.should(outputPath != null, "output file not specified");
         Log.d("outputPath: " + outputPath);
 
+        if (Boolean.TRUE.equals(Flag.get("-S"))) {
+            final var irPath = sourcePath.replace(".sy", ".ll");
+            AssemblyBuilder assemblyBuilder = new AssemblyBuilder(irPath);
+            assemblyBuilder.writeRaw(context.getCurrentIR().toString());
+            assemblyBuilder.close();
+        }
+
         AssemblyBuilder assemblyBuilder = new AssemblyBuilder(outputPath);
 
         assemblyBuilder.writeRaw(context.getCurrentIR().toString());
+        assemblyBuilder.close();
     }
 
     public static void parseSource(IceContext context) throws IOException {
@@ -51,35 +55,5 @@ public class Compiler {
         final var tokenStream = new CommonTokenStream(lexer);
         final var parser = new SysyParser(tokenStream);
         context.setAst(parser.compUnit());
-    }
-
-    public static void writeIR() throws IOException {
-        final String outputPath = Flag.get("-o");
-        Log.should(outputPath != null, "output file not specified");
-        Log.d("outputPath: " + outputPath);
-
-        AssemblyBuilder assemblyBuilder = new AssemblyBuilder(outputPath);
-
-        // Write the assembly code to the file
-        assemblyBuilder.writeRaw(".syntax unified\n");
-        assemblyBuilder.writeRaw(".arch armv7-a\n");
-        assemblyBuilder.writeRaw(".fpu vfpv4\n");
-        assemblyBuilder.writeRaw(".global main\n");
-        assemblyBuilder.writeRaw(".text\n");
-        assemblyBuilder.writeRaw(".align 2\n");
-        assemblyBuilder.writeRaw(".type main, %function\n");
-        assemblyBuilder.writeRaw("main:\n");
-        assemblyBuilder.writeRaw("push {fp, lr}\n");
-        assemblyBuilder.writeRaw("add fp, sp, #4\n");
-        assemblyBuilder.writeRaw("mov r0, #0\n");
-        assemblyBuilder.writeRaw("SYS_Y_0:\n");
-        assemblyBuilder.writeRaw("sub sp, fp, #4\n");
-        assemblyBuilder.writeRaw("pop {fp, pc}\n");
-        assemblyBuilder.writeRaw("\n");
-        assemblyBuilder.writeRaw(".section\t.note.GNU-stack,\"\",%progbits\n");
-        assemblyBuilder.writeRaw(".ident\t\"SysY-Compiler\"\n");
-
-        // Close the file
-        assemblyBuilder.close();
     }
 }
