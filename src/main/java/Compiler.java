7672726d32f934243544e33ff9f21d80a1f538fc
null <30485571+nullnan@users.noreply.github.com>
2025-04-07 15:37:49 +0800

    把Compiler的main函数分解到对象中

--- src/main/java/Compiler.java
+++ src/main/java/Compiler.java
@@ -10,50 +10,63 @@ import top.voidc.misc.Log;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Compiler {
-    public static void main(String[] args) throws IOException {
-        Flag.init(args);
-        final String sourcePath = Flag.get("source");
-        Log.should(sourcePath != null, "source file not specified");
+    public final String sourcePath;
+    public final String outputPath;
+    public final File source;
+    public final IceContext context = new IceContext();
+
+    public Compiler(String sourcePath, String outputPath) {
+        this.sourcePath = Objects.requireNonNull(sourcePath);
+        this.outputPath = Objects.requireNonNull(outputPath);
+
         Log.d("sourcePath: " + sourcePath);
+        Log.d("outputPath: " + outputPath);
 
-        IceContext context = new IceContext();
-
-        File source = new File(sourcePath);
-
+        this.source = new File(sourcePath);
         Log.should(source.exists(), "source file does not exist");
 
         context.setSource(source);
+    }
 
+    public void compile() throws IOException {
         parseSource(context);
 
         IRGenerator generator = new IRGenerator(context);
-
         generator.generateIR();
 
-        final String outputPath = Flag.get("-o");
-        Log.should(outputPath != null, "output file not specified");
-        Log.d("outputPath: " + outputPath);
+        emitLLVM();
 
+        AssemblyBuilder assemblyBuilder = new AssemblyBuilder(outputPath);
+        assemblyBuilder.writeRaw(context.getCurrentIR().toString());
+        assemblyBuilder.close();
+    }
+
+    public void parseSource(IceContext context) throws IOException {
+        final var input = CharStreams.fromFileName(context.getSource().getAbsolutePath());
+        final var lexer = new SysyLexer(input);
+        final var tokenStream = new CommonTokenStream(lexer);
+        final var parser = new SysyParser(tokenStream);
+        context.setAst(parser.compUnit());
+    }
+
+    public void emitLLVM() throws IOException {
         if (Boolean.TRUE.equals(Flag.get("-S"))) {
             final var irPath = sourcePath.replace(".sy", ".ll");
             AssemblyBuilder assemblyBuilder = new AssemblyBuilder(irPath);
             assemblyBuilder.writeRaw(context.getCurrentIR().toString());
             assemblyBuilder.close();
         }
-
-        AssemblyBuilder assemblyBuilder = new AssemblyBuilder(outputPath);
-
-        assemblyBuilder.writeRaw(context.getCurrentIR().toString());
-        assemblyBuilder.close();
     }
 
-    public static void parseSource(IceContext context) throws IOException {
-        final var input = CharStreams.fromFileName(context.getSource().getAbsolutePath());
-        final var lexer = new SysyLexer(input);
-        final var tokenStream = new CommonTokenStream(lexer);
-        final var parser = new SysyParser(tokenStream);
-        context.setAst(parser.compUnit());
+    public static void main(String[] args) throws IOException {
+        Flag.init(args);
+        final String sourcePath = Flag.get("source");
+        final String outputPath = Flag.get("-o");
+
+        final var compiler = new Compiler(sourcePath, outputPath);
+        compiler.compile();
     }
 }
