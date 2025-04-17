e92cb11d84d5a7fe59825c5d5bdaa15fc9fc3360
SamBillon <89698096+SamBillon@users.noreply.github.com>
2025-04-17 16:44:04 -0500

    upd: 进一步完善 SCCP

--- src/test/java/top/voidc/optimizer/pass/function/SparseConditionalConstantPropagationTest.java
+++ src/test/java/top/voidc/optimizer/pass/function/SparseConditionalConstantPropagationTest.java
@@ -59,4 +59,71 @@ public class SparseConditionalConstantPropagationTest {
                 "}";
         assertEquals(expected, actual.toString());
     }
+
+    private static IceFunction createComplexFunction() {
+        IceFunction function = new IceFunction("testFunction");
+        function.setReturnType(IceType.I32);
+        IceValue a = new IceValue("a", IceType.I32);
+        IceValue b = new IceValue("b", IceType.I32);
+        function.addParameter(a);
+        function.addParameter(b);
+
+        IceBlock entry = function.getEntryBlock();
+        IceBlock block1 = new IceBlock(function, "block1");
+        IceBlock block2 = new IceBlock(function, "block2");
+        IceBlock exit = function.getExitBlock();
+
+        IceInstruction icmp1 = new IceIcmpInstruction(entry, IceIcmpInstruction.CmpType.EQ,
+                new IceConstantInt(1), new IceConstantInt(2));
+        IceInstruction outEntry = new IceBranchInstruction(entry, icmp1, block1, block2);
+        entry.addInstruction(icmp1);
+        entry.addInstruction(outEntry);
+
+        IceInstruction sub = new IceBinaryInstruction(block2, InstructionType.SUB, "sub", IceType.I32,
+                a, b);
+        IceInstruction outBlock1 = new IceBranchInstruction(block1, exit);
+        block1.addInstruction(sub);
+        block1.addInstruction(outBlock1);
+
+        IceInstruction add = new IceBinaryInstruction(block1, InstructionType.ADD, "add", IceType.I32,
+                new IceConstantInt(1), new IceConstantInt(2));
+        IceInstruction outBlock2 = new IceBranchInstruction(block2, exit);
+        block2.addInstruction(add);
+        block2.addInstruction(outBlock2);
+
+        IcePHINode phi = new IcePHINode(exit, "phi", IceType.I32);
+        phi.addBranch(block1, sub);
+        phi.addBranch(block2, add);
+        exit.addInstruction(phi);
+        IceInstruction ret = new IceRetInstruction(exit, phi);
+        exit.addInstruction(ret);
+
+        return function;
+    }
+
+    @Test
+    public void testComplexSCCP() {
+        SparseConditionalConstantPropagation pass = new SparseConditionalConstantPropagation();
+        IceFunction function = createComplexFunction();
+
+        StringBuilder before = new StringBuilder();
+        function.getTextIR(before);
+
+        pass.run(function);
+
+        StringBuilder actual = new StringBuilder();
+        function.getTextIR(actual);
+
+        Log.d("Before:\n" + before.toString() + "\nAfter:\n" + actual.toString());
+
+        String expected = "define i32 @testFunction(i32 %a, i32 %b) {\n" +
+                "entry:\n" +
+                "block1:\n" +
+                "block2:\n" +
+                "exit:\n" +
+                "\tret i32 3\n" +
+                "\n" +
+                "}";
+        assertEquals(expected, actual.toString());
+    }
 }
