aaa151cbb0d65fa59e98974c0d0078dea9d04c4c
SamBillon <89698096+SamBillon@users.noreply.github.com>
2025-04-18 02:04:00 -0500

    fix: 将一个 remove 修改为 destroy

--- src/main/java/top/voidc/optimizer/pass/function/SmartChilletSimplifyCFG.java
+++ src/main/java/top/voidc/optimizer/pass/function/SmartChilletSimplifyCFG.java
@@ -241,7 +241,7 @@ public class SmartChilletSimplifyCFG implements CompilePass<IceFunction> {
                 }
             }
 
-            parent.removeInstruction(binary);
+            binary.destroy();
         }
     }
 
