b7f8edad02541d2cb1a23f6c8cd62c25003776a3
L-Bromine <lishaofan_31415926@163.com>
2025-04-27 01:02:53 +0800

    merge null

---
+++ src/main/java/top/voidc/optimizer/pass/function/ScalarReplacementOfAggregates.java
@@ -0,0 +1,149 @@
+package top.voidc.optimizer.pass.function;
+
+import top.voidc.ir.IceBlock;
+import top.voidc.ir.IceValue;
+import top.voidc.ir.ice.constant.IceConstant;
+import top.voidc.ir.ice.constant.IceConstantInt;
+import top.voidc.ir.ice.constant.IceFunction;
+import top.voidc.ir.ice.instruction.IceAllocaInstruction;
+import top.voidc.ir.ice.instruction.IceGEPInstruction;
+import top.voidc.ir.ice.instruction.IceInstruction;
+import top.voidc.ir.ice.type.IceArrayType;
+import top.voidc.ir.ice.type.IcePtrType;
+import top.voidc.ir.ice.type.IceType;
+import top.voidc.optimizer.pass.Pass;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ * 标量替换聚合
+ * 将 聚合类型 的变量（数组）替换为多个标量变量
+ */
+public class ScalarReplacementOfAggregates implements Pass<IceFunction> {
+    private static ArrayList<IceAllocaInstruction> createPromotableList(IceFunction function) {
+        ArrayList<IceAllocaInstruction> result = new ArrayList<>();
+        for (IceBlock block : function.getBlocks()) {
+            for (IceInstruction instr : block.getInstructions()) {
+                if (instr instanceof IceAllocaInstruction alloca) {
+                    IceType type = ((IcePtrType<?>) alloca.getType()).getPointTo();
+
+                    if (!(type instanceof IceArrayType arrayType)) {
+                        continue;
+                    }
+
+                    if (arrayType.getDimSize() > 1) {
+                        // 长大后再学习
+                        continue;
+                    }
+                    if (arrayType.getTotalSize() > 10) {
+                        // 太大的数组不支持，防止代码过于复杂
+                        continue;
+                    }
+
+                    result.add(alloca);
+                } else if (instr instanceof IceGEPInstruction gep) {
+                    // 这个 pass 只能处理被 静态访问 的数组，因此需要检查 gep 的下标是不是全为 constant
+                    List<IceValue> indices = gep.getOperandsList();
+                    for (int i = 1; i < indices.size(); i++) {
+                        IceValue index = indices.get(i);
+                        if (!(index instanceof IceConstant)) {
+                            // 如果下标不是常量，则删除对应的 alloca
+                            IceValue basePtr = gep.getBasePtr();
+                            if (basePtr instanceof IceAllocaInstruction alloca) {
+                                result.remove(alloca);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 在 function 开头展开 alloca 指令，并返回新的指令列表
+     *
+     * @param aggregate 聚合类型的 alloca 指令
+     * @return 新的指令列表
+     */
+    private ArrayList<IceValue> aggregatesExpansion(IceAllocaInstruction aggregate) {
+        // type 的类型转换
+        IceArrayType arrayType = (IceArrayType) (((IcePtrType<?>) aggregate.getType()).getPointTo());
+        IceType elementType = arrayType.getElementType();
+
+        ArrayList<IceValue> result = new ArrayList<>();
+        // 展开 alloca，并且删除原指令
+        for (int i = 0; i < arrayType.getNumElements(); i++) {
+            IceAllocaInstruction newAlloca = new IceAllocaInstruction(aggregate.getParent(), aggregate.getName() + ".i" + i, elementType);
+            aggregate.getParent().addInstructionAtFront(newAlloca);
+            result.add(newAlloca);
+        }
+
+        aggregate.getParent().removeInstruction(aggregate);
+
+        return result;
+    }
+
+    private static void replaceGEPInstructions(IceFunction function, Hashtable<IceAllocaInstruction, ArrayList<IceValue>> allocaLists) {
+        // 别名表：IceValue -> 它的别名
+        Hashtable<IceValue, IceValue> aliasTable = new Hashtable<>();
+
+        for (IceBlock block : function.getBlocks()) {
+            List<IceInstruction> instructions = block.getInstructions();
+            for (int index = 0; index < instructions.size(); ++index) {
+                IceInstruction instr = instructions.get(index);
+                if (instr instanceof IceGEPInstruction gep) {
+                    IceValue basePtr = gep.getBasePtr();
+
+                    if (!(basePtr instanceof IceAllocaInstruction alloca)) {
+                        throw new RuntimeException("GEP base pointer is not an alloca instruction");
+                    }
+
+                    if (!allocaLists.containsKey(alloca)) {
+                        continue;
+                    }
+
+                    ArrayList<IceValue> newAllocaList = allocaLists.get(alloca);
+
+                    // TODO: 如果做高维数组展开，这个地方可能要替换
+                    // 由于 promotableList 中的 alloca 都只被静态使用，所以可以直接转换
+                    IceConstantInt arrayIndex = (IceConstantInt) gep.getOperand(2);
+                    IceValue newAlloca = newAllocaList.get((int) arrayIndex.getValue());
+
+                    // gep 相当于设置了一个别名，因此将 gep 的别名设置为 newAlloca
+                    aliasTable.put(gep, newAlloca);
+
+                    // 删除 gep
+                    block.removeInstruction(gep);
+                    // 调整 index
+                    --index;
+                } else {
+                    // 如果不是 gep，应当尝试替换指令中的别名
+                    List<IceValue> operands = instr.getOperandsList();
+                    for (int i = 0; i < operands.size(); ++i) {
+                        IceValue operand = instr.getOperand(i);
+                        if (aliasTable.containsKey(operand)) {
+                            IceValue newOperand = aliasTable.get(operand);
+                            instr.setOperand(i, newOperand);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void run(IceFunction target) {
+        ArrayList<IceAllocaInstruction> promotableList = createPromotableList(target);
+        Hashtable<IceAllocaInstruction, ArrayList<IceValue>> newAllocaLists = new Hashtable<>();
+        for (IceAllocaInstruction alloca : promotableList) {
+            ArrayList<IceValue> newAllocaList = aggregatesExpansion(alloca);
+            newAllocaLists.put(alloca, newAllocaList);
+        }
+
+        replaceGEPInstructions(target, newAllocaLists);
+    }
+}
