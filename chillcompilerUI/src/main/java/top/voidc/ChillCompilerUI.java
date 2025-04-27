260db42db8d2a9c4a007b849b1c40cd5bd7951df
L-Bromine <lishaofan_31415926@163.com>
2025-04-27 13:25:01 +0800

    feat：添加父pom和ui子模块到目录

---
+++ chillcompilerUI/src/main/java/top/voidc/ChillCompilerUI.java
@@ -0,0 +1,12 @@
+package top.voidc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillCompilerUI {
+    public static void main(String[] args) {
+        SpringApplication.run(ChillCompilerUI.class, args);
+    }
+}
