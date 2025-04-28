eb844744fca174588f7f30e9a217c98b6f378779
L-Bromine <lishaofan_31415926@163.com>
2025-04-29 06:10:28 +0800

    feat：通过配置添加组件

--- chillcompilerUI/src/main/java/top/voidc/controller/MainController.java
+++ chillcompilerUI/src/main/java/top/voidc/controller/MainController.java
@@ -1,40 +1,32 @@
 package top.voidc.controller;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import top.voidc.service.ComponentsService;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    ComponentsService componentsService;
+
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("componentsList", List.of(new String[]{"sysY.js", "llvm.js"}));
+    public String home(Model model) throws IOException {
+        model.addAttribute("componentsList", componentsService.getFilesWithComments("js/components"));
         return "index";
     }
 
-    // 配置组件目录（根据实际路径调整）
-    private static final String COMP_DIR = "static/js/components/";
-
-    @GetMapping("/api/sysycompiler")
-    @ResponseBody
-    public String sysycompiler(String content) {
-        return content;
-    }
-
+    /**
+     * 重定向editor
+     */
     @GetMapping("/vs/{*path}")
     public void getEditorMain(HttpServletResponse response, @PathVariable("path") String path) throws IOException {
-        System.out.println(path);
         response.sendRedirect("/js/monaco-editor/min/vs/" + path);
     }
 }
