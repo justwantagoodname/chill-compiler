8d751784d7466f8fa0aa24600304d6ca8a7e42fa
L-Bromine <lishaofan_31415926@163.com>
2025-04-28 18:57:35 +0800

    feat：前端暂时能用了

--- chillcompilerUI/src/main/java/top/voidc/ChillCompilerUI.java
+++ chillcompilerUI/src/main/java/top/voidc/ChillCompilerUI.java
@@ -1,6 +1,5 @@
 package top.voidc;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ chillcompilerUI/src/main/resources/static/js/mainbox.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    // 配置布局
+    const config = {
+        content: [{
+            type: 'row',
+            content: [{
+                    type: 'component',
+                    componentName: 'sysY',
+                    title: 'sysY Editor',
+                },
+                {
+                    type: 'component',
+                    componentName: 'llvm',
+                    title: 'llvm show',
+                }
+            ]
+        }]
+    };
+
+    // 创建布局实例
+    const layout = new GoldenLayout(config, '#layout-container');
+
+    // 从component中注册组件
+    registerComponents(layout)
+
+    // 初始化布局
+    layout.init();
+});
+
+var components_register = []
+
+async function registerComponents(layout) {
+    components_register.forEach(component => {
+        component(layout)
+    })
+}
