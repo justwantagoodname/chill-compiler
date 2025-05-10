68bb9d837a2c594cc0ce879a2c1574526e8fed7b
null <30485571+nullnan@users.noreply.github.com>
2025-05-10 23:24:07 +0800

    feat: undef新增指针类型

--- src/main/java/top/voidc/ir/ice/constant/IceUndef.java
+++ src/main/java/top/voidc/ir/ice/constant/IceUndef.java
@@ -1,15 +1,18 @@
 package top.voidc.ir.ice.constant;
 
+import top.voidc.ir.ice.type.IcePtrType;
 import top.voidc.ir.ice.type.IceType;
 
 public class IceUndef extends IceConstant {
     public static final IceUndef I32 = new IceUndef(IceType.I32);
     public static final IceUndef F32 = new IceUndef(IceType.F32);
+    public static final IceUndef PTR = new IceUndef(new IcePtrType<>(IceType.VOID));
 
     public static IceUndef get(IceType type) {
         return switch (type.getTypeEnum()) {
             case I32 -> I32;
             case F32 -> F32;
+            case PTR -> PTR;
             default -> throw new IllegalArgumentException("Unknown undef type: " + type);
         };
     }
