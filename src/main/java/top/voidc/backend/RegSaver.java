b57ee71df0de983f1596aae613604adbcd7e20b7
SamBillon <89698096+SamBillon@users.noreply.github.com>
2025-07-29 13:36:29 +0800

    upd: 逻辑写完了，但是 getExitBlock 实现有问题

--- src/main/java/top/voidc/backend/RegSaver.java
+++ src/main/java/top/voidc/backend/RegSaver.java
@@ -8,6 +8,7 @@ import top.voidc.ir.machine.IceMachineInstruction;
 import top.voidc.ir.machine.IceMachineRegister;
 
 import top.voidc.ir.machine.IceStackSlot;
+import top.voidc.misc.Log;
 import top.voidc.misc.annotation.Pass;
 import top.voidc.optimizer.pass.CompilePass;
 
@@ -26,17 +27,35 @@ public class RegSaver implements CompilePass<IceMachineFunction>, IceArchitectur
         };
     }
 
+    private static boolean isCalleeSaved(IceMachineRegister reg) {
+        String name = reg.getName();
+        if (name.equals("zr")) return false;
+        int id = Integer.parseInt(name);
+        return switch (id) {
+            case 19, 20, 21, 22, 23, 24, 25, 26, 27 -> true; // Callee-saved registers
+            default -> false; // Other registers are caller-saved
+        };
+    }
+
     @Override
     public boolean run(IceMachineFunction target) {
         if (!(target instanceof ARM64Function mf)) {
             throw new IllegalArgumentException("CallerSaver only supports ARM64Function.");
         }
 
+        saveCallerSavedRegs(mf);
+//        saveCalleeSavedRegs(mf);
+
+        return true;
+    }
+
+    private void saveCallerSavedRegs(ARM64Function mf) {
+
         // 直到这次被调用前，使用过的 caller-save 寄存器集合
         Set<IceMachineRegister> usedRegs = new HashSet<>();
         Map<IceMachineRegister, IceStackSlot> regSlots = new HashMap<>();
 
-        for (var block : target) {
+        for (var block : mf) {
             for (int i = 0; i < block.size(); ++i) {
                 if (!(block.get(i) instanceof IceMachineInstruction inst)) {
                     throw new IllegalArgumentException("Why is there a non-machine instruction in a machine function?");
@@ -73,8 +92,43 @@ public class RegSaver implements CompilePass<IceMachineFunction>, IceArchitectur
                 }
             }
         }
+    }
 
-        return true;
+    private void saveCalleeSavedRegs(ARM64Function mf) {
+        // 直到这次被调用前，使用过的 callee-save 寄存器集合
+        Set<IceMachineRegister> usedRegs = new HashSet<>();
+        Map<IceMachineRegister, IceStackSlot> regSlots = new HashMap<>();
+
+        for (var block : mf) {
+            for (int i = 0; i < block.size(); ++i) {
+                if (!(block.get(i) instanceof IceMachineInstruction inst)) {
+                    throw new IllegalArgumentException("Why is there a non-machine instruction in a machine function?");
+                }
+
+                for (var operand : inst.getOperands()) {
+                    if (operand instanceof IceMachineRegister.RegisterView rv) {
+                        IceMachineRegister reg = rv.getRegister();
+                        assert !reg.isVirtualize() : "Virtual registers should not be present in machine instructions after register allocation.";
+                        if (!isCalleeSaved(reg)) {
+                            // 如果是 callee-saved 寄存器，记录下来
+                            usedRegs.add(reg);
+                            regSlots.putIfAbsent(reg, mf.allocateVariableStackSlot(reg.getType()));
+                        }
+                    }
+                }
+            }
+        }
+
+        var entryBlock = mf.getEntryBlock();
+        var exitBlock = mf.getExitBlock();
+
+        for (var reg : usedRegs) {
+            IceMachineInstruction str = new ARM64Instruction("STR {src}, {local:dst}", reg.createView(reg.getType()), regSlots.get(reg));
+            IceMachineInstruction ldr = new ARM64Instruction("LDR {src}, {local:dst}", reg.createView(reg.getType()), regSlots.get(reg));
+            entryBlock.add(0, str);
+            Log.d(exitBlock.getTextIR());
+            exitBlock.add(exitBlock.size() - 1, ldr);
+        }
     }
 
     @Override
