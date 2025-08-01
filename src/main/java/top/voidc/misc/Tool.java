a01a827c5643ced194a894ebee6c24ced6f9639a
null <30485571+nullnan@users.noreply.github.com>
2025-08-01 23:17:37 +0800

    refactor: 寄存器类型判断放到Tool里面了，因为很常用

--- src/main/java/top/voidc/misc/Tool.java
+++ src/main/java/top/voidc/misc/Tool.java
@@ -1,8 +1,11 @@
 package top.voidc.misc;
 
+import top.voidc.backend.arm64.instr.ARM64Register;
 import top.voidc.ir.ice.instruction.IceCmpInstruction;
+import top.voidc.ir.ice.type.IceType;
+import top.voidc.ir.ice.type.IceVecType;
+import top.voidc.ir.machine.IceMachineRegister;
 
-import java.util.Base64;
 import java.util.List;
 
 public class Tool {
@@ -88,5 +91,48 @@ public class Tool {
             sb.append(String.format("\\%03o", b));
         }
         return sb.toString();
+    }
+
+    public enum RegisterType {
+        CALLER_SAVED, // 调用者保存寄存器
+        CALLEE_SAVED,  // 被调用者保存寄存器
+        READ_ONLY,         // 零寄存器
+    }
+
+    public static RegisterType getArm64RegisterType(IceMachineRegister reg) {
+        assert reg instanceof ARM64Register;
+        assert reg.getType().equals(IceType.I64) || reg.getType().equals(IceVecType.VEC128);
+
+        if (reg.getType().equals(IceType.I64)) {
+            // ARM64 的整数寄存器
+            String name = reg.getName();
+            if (name.equals("zr")) return RegisterType.READ_ONLY; // 零寄存器
+            if (name.equals("sp") || name.equals("fp") || name.equals("lr")) {
+                return RegisterType.CALLEE_SAVED; // sp, fp, lr 是被调用者保存寄存器
+            }
+
+            int id = Integer.parseInt(name);
+            if (Tool.inRange(id, 0, 18)) { // x0 - x18
+                return RegisterType.CALLER_SAVED; // Caller-saved registers
+            } else if (Tool.inRange(id, 19, 27)) { // x19 - x28 x29(fp) x30(lr)
+                return RegisterType.CALLEE_SAVED;
+            } else {
+                throw new IllegalArgumentException("Unknown register: " + name);
+            }
+        } else if (reg.getType().equals(IceVecType.VEC128)) {
+            var name = reg.getName();
+            var id = Integer.parseInt(name);
+            if (Tool.inRange(id, 0, 7) || Tool.inRange(id, 16, 31)) { // v0 - v7 v16 - v31
+                return RegisterType.CALLER_SAVED; // Caller-saved vector registers
+            } else if (Tool.inRange(id, 8, 15)) { // v8 - v15
+                // Note: 理论上只有低 64 位是 callee-saved 的
+                // 但为了简化处理，这里将 v8 - v15 也视为 callee-saved
+                // 这意味着在使用这些寄存器时需要保存全部 128 位没有什么影响
+                return RegisterType.CALLEE_SAVED; // Callee-saved vector registers
+            } else {
+                throw new IllegalArgumentException("Unknown vector register: " + name);
+            }
+        }
+        throw new IllegalStateException("Unknown register type: " + reg.getType());
     }
 }
