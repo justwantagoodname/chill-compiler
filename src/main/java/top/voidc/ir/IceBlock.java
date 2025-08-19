1f04c0fd8770d588ff0383a401ca6e7d91fa36ce
null <30485571+nullnan@users.noreply.github.com>
2025-08-20 03:27:23 +0800

    fix: 尝试修复了一下IceBlock的getPred

--- src/main/java/top/voidc/ir/IceBlock.java
+++ src/main/java/top/voidc/ir/IceBlock.java
@@ -118,7 +118,7 @@ public class IceBlock extends IceUser implements List<IceInstruction> {
 
     public List<IceBlock> getPredecessors() {
         return getUsers().stream()
-                .filter(iceUser -> iceUser instanceof IceInstruction)
+                .filter(iceUser -> iceUser instanceof IceInstruction inst && inst.isTerminal())
                 .map(inst -> ((IceInstruction) inst).getParent()).toList();
     }
 
